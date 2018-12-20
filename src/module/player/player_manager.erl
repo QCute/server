3287a0e5bb7ac8f0c8261e6c8ce5a0f42de0ff1a
QCute <baldoor@qq.com>
2018-12-20 19:38:32 +0800

    q

--- src/module/player/player_manager.erl
+++ src/module/player/player_manager.erl
@@ -6,7 +6,7 @@
 -module(player_manager).
 -behaviour(gen_server).
 %% export API function
--export([start/0, start_link/1]).
+-export([start/0, start_link/0]).
 -export([is_online/1, get_user_pid/1, lookup/1, broadcast/1]).
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
@@ -21,8 +21,8 @@ start() ->
     process:start(?MODULE).
 
 %% @doc gen_server entry
-start_link(Args) ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 %% @doc user online
 is_online(UserId) ->
