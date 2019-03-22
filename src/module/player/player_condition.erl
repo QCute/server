e7baaa9444f7436255ec01b6c3e5f42b82a0d7d6
baldoor <baldoor@qq.com>
2019-03-22 19:40:12 +0800

    q

--- src/module/player/player_condition.erl
+++ src/module/player/player_condition.erl
@@ -47,6 +47,59 @@ check(User = #user{player = #player{sex = Sex}}, [{sex, Sex, _} | T]) ->
     check(User, T);
 check(User = #user{player = #player{classes = Classes}}, [{classes, Classes, _} | T]) ->
     check(User, T);
+
+check(User, [{eq, X, X} | T]) ->
+    check(User, T);
+check(User, [{ne, X, Y} | T]) when X =/= Y ->
+    check(User, T);
+check(User, [{gt, X, Y} | T]) when X > Y ->
+    check(User, T);
+check(User, [{lt, X, Y} | T]) when X < Y ->
+    check(User, T);
+check(User, [{ge, X, Y} | T]) when X >= Y ->
+    check(User, T);
+check(User, [{le, X, Y} | T]) when X =< Y ->
+    check(User, T);
+
+check(User, [{eq, X, X, _} | T]) ->
+    check(User, T);
+check(User, [{ne, X, Y, _} | T]) when X =/= Y ->
+    check(User, T);
+check(User, [{gt, X, Y, _} | T]) when X > Y ->
+    check(User, T);
+check(User, [{lt, X, Y, _} | T]) when X < Y ->
+    check(User, T);
+check(User, [{ge, X, Y, _} | T]) when X >= Y ->
+    check(User, T);
+check(User, [{le, X, Y, _} | T]) when X =< Y ->
+    check(User, T);
+
+check(_User, [{r, eq, X, X} | _]) ->
+    {error, 0};
+check(_User, [{r, ne, X, Y} | _]) when X =/= Y ->
+    {error, 0};
+check(_User, [{r, gt, X, Y} | _]) when X > Y ->
+    {error, 0};
+check(_User, [{r, lt, X, Y} | _]) when X < Y ->
+    {error, 0};
+check(_User, [{r, ge, X, Y} | _]) when X >= Y ->
+    {error, 0};
+check(_User, [{r, le, X, Y} | _]) when X =< Y ->
+    {error, 0};
+
+check(_User, [{r, eq, X, X, Code} | _]) ->
+    {error, Code};
+check(_User, [{r, ne, X, Y, Code} | _]) when X =/= Y ->
+    {error, Code};
+check(_User, [{r, gt, X, Y, Code} | _]) when X > Y ->
+    {error, Code};
+check(_User, [{r, lt, X, Y, Code} | _]) when X < Y ->
+    {error, Code};
+check(_User, [{r, ge, X, Y, Code} | _]) when X >= Y ->
+    {error, Code};
+check(_User, [{r, le, X, Y, Code} | _]) when X =< Y ->
+    {error, Code};
+
 %% return error code
 check(_, [{_, _, Code} | _]) ->
     {error, Code};
@@ -57,3 +110,11 @@ check(_, _) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+%% normal compare check mode: (if condition true, continue, else return error with code(if given))
+%% with (r) reverse mode: (if condition true, return error with code(if given), else continue)
+%% eq     ==     equal
+%% ne     =/=    not equal
+%% gt     >      greater than
+%% lt     <      less than
+%% ge     >=     greater than or equal
+%% le     =<     less than or equal
