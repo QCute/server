9dfa9383718a62dd9defd464ecaae5c586c2de8f
QCute <759276607@qq.com>
2024-12-17 19:51:57 +0800

    Untitled Commit

--- src/tool/assistant/record.erl
+++ src/tool/assistant/record.erl
@@ -13,7 +13,6 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 %% Includes
 -include_lib("stdlib/include/ms_transform.hrl").
--include("time.hrl").
 %%%===================================================================
 %%% API functions
 %%%===================================================================
